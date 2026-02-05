c5c83d706a019eaca3acb34db1e7c625afbc2180
Estefanie <estefaniesalazar041@gmail.com>
2026-02-05 14:15:50 -0600

    Add user and login data structures in data.py

---
+++ data.py
@@ -0,0 +1,24 @@
+USER_DATA = {
+    "name": "Daniel Rivera",
+    "job": "Backend Developer"
+}
+
+UPDATED_USER_DATA = {
+    "name": "Daniel Rivera QA",
+    "job": "Automation Engineer"
+}
+
+SECOND_USER_DATA = {
+    "name": "Mariana López",
+    "job": "UX Designer"
+}
+
+LOGIN_VALID = {
+    "email": "eve.holt@reqres.in",
+    "password": "cityslicka"
+}
+
+LOGIN_INVALID = {
+    "email": "jorge@test.com",
+    "password": ""
+}
